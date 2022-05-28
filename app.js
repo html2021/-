7dd99734d6330bfe8fedd36a896537e5386e4804
html2021 <78289310+html2021@users.noreply.github.com>
2022-05-29 08:58:35 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-timegame = true;
-time = 0;
-function d12456() {
-	time++;
-}
-function timee134() {
-	while (timegame) {
-		window.setTimeout(d12456, 1000);
-	}
-}
+var timer    = null,
+    textObj  = null,
+    execBtn  = null,
+    clearBtn = null,
+    n        = 0;
+
+var startIntervalFunc = function() {
+	if (!timer) timer = setInterval(execIntervalFunc, 1000);
+};
+
+var execIntervalFunc = function() {
+	n++;
+	textObj.value = n;
+};
+
+var clearIntervalFunc = function() {
+	// setIntervalの停止
+	clearInterval(timer);
+	time = null;
+	n = 0;
+	textObj.value = n;
+};
+
+textObj  = document.getElementById('text');
+execBtn  = document.getElementById('exec');
+clearBtn = document.getElementById('clear');
+
+execBtn.onclick  = startIntervalFunc;
+clearBtn.onclick = clearIntervalFunc;
 function start(){
   document.getElementById("time").innerText = "変更しました";
   timee134();
 }
 function stop() {
-	document.getElementById("time").innerText = time;
+	document.getElementById("time").innerText = n;
 	timegame = false;
 }
