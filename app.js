c24938f81852744429ea603c836993d56d7b914c
html2021 <78289310+html2021@users.noreply.github.com>
2022-05-30 08:03:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-var timer    = null,
-    textObj  = null,
-    execBtn  = null,
-    clearBtn = null,
-    n        = 0;
-
-var startIntervalFunc = function() {
-	if (!timer) timer = setInterval(execIntervalFunc, 1000);
-};
-
-var execIntervalFunc = function() {
-	n++;
-	textObj.value = n;
-};
-
-var clearIntervalFunc = function() {
-	// setIntervalの停止
-	clearInterval(timer);
-	time = null;
-	n = 0;
-	textObj.value = n;
-};
-
-textObj  = document.getElementById('text');
-execBtn  = document.getElementById('exec');
-clearBtn = document.getElementById('clear');
-
-execBtn.onclick  = startIntervalFunc;
-clearBtn.onclick = clearIntervalFunc;
+n = 0;
+funtion time() {
+n++;
+}
+setlnterval("time()", 1000)
 function start(){
   document.getElementById("time").innerText = "変更しました";
   timee134();
